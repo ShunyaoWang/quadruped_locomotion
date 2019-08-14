2d2e64f895af94f11afbd2dd367e5256be6a0440
Shunyao Wang <wshy1995@outlook.com>
2019-08-14 10:36:26 +0800

    fix merge mistakes

---
+++ free_gait_core/include/free_gait_core/executor/executor.h
@@ -0,0 +1,17 @@
+/*
+ * executor.hpp
+ *
+ *  Created on: Oct 22, 2015
+ *      Author: Péter Fankhauser
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#pragma once
+
+#include "free_gait_core/executor/AdapterBase.hpp"
+#include "free_gait_core/executor/Executor.hpp"
+#include "free_gait_core/executor/BatchExecutor.hpp"
+#include "free_gait_core/executor/ExecutorState.hpp"
+#include "free_gait_core/executor/State.hpp"
+#include "free_gait_core/executor/StateBatch.hpp"
+#include "free_gait_core/executor/StateBatchComputer.hpp"
